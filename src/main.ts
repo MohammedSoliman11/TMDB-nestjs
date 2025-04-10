7c7dc46546ef80384151a34750917d506d1b9629
MohammedSoliman11 <mohammedsoliman404@gmail.com>
2025-04-10 23:49:30 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TMDB API')
+    .setDescription('The TMDB API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3333);
+}
+
+bootstrap();
